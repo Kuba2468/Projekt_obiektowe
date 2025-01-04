963d1e9715b5ae9028bb74983a2de389be8f3290
Jakub Czechowicz <w69932@student.wsiz.edu.pl>
2025-01-04 13:08:17 +0100

    service klient

--- Bileciki/Services/KlientService.cs
+++ Bileciki/Services/KlientService.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
-namespace Bileciki.Services
+public class KlientService : IKlientService
 {
-    internal class KlientService
+    private Database db;
+
+    public KlientService(Database database)
     {
+        db = database;
+    }
+
+    public void AddKlient(Klient klient)
+    {
+        db.Klienci.Add(klient);
+    }
+
+    public Klient GetKlient(int id)
+    {
+        return db.Klienci.FirstOrDefault(k => k.Id == id);
+    }
+
+    public void UpdateKlient(Klient klient)
+    {
+        var item = GetKlient(klient.Id);
+        if (item != null)
+        {
+            item.Imie = klient.Imie;
+            item.Nazwisko = klient.Nazwisko;
+        }
+    }
+
+    public void DeleteKlient(int id)
+    {
+        var item = GetKlient(id);
+        if (item != null)
+        {
+            db.Klienci.Remove(item);
+        }
     }
 }
