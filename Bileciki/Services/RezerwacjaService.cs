a0dd02e9c6ae806e154682974dee9f9d62a90a4f
Jakub Czechowicz <w69932@student.wsiz.edu.pl>
2025-01-06 15:06:20 +0100

    servis rezerwacja

--- Bileciki/Services/RezerwacjaService.cs
+++ Bileciki/Services/RezerwacjaService.cs
@@ -1,12 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Bileciki.Services
+public class RezerwacjaService : IRezerwacjaService
 {
-    internal class RezerwacjaService
+    private Database db;
+
+    public RezerwacjaService(Database database)
     {
+        db = database;
+    }
+
+    public void AddRezerwacja(Rezerwacja rezerwacja)
+    {
+        if (rezerwacja.DataRezerwacji <= DateTime.Now)
+        {
+            throw new Exception("Data musi być w przyszłości.");
+        }
+        db.Rezerwacje.Add(rezerwacja);
+    }
+
+    public Rezerwacja GetRezerwacja(int id)
+    {
+        return db.Rezerwacje.FirstOrDefault(r => r.Id == id);
+    }
+
+    public void UpdateRezerwacja(Rezerwacja rezerwacja)
+    {
+        var item = GetRezerwacja(rezerwacja.Id);
+        if (item != null)
+        {
+            item.Film = rezerwacja.Film;
+            item.DataRezerwacji = rezerwacja.DataRezerwacji;
+            item.Bilet = rezerwacja.Bilet;
+        }
+    }
+
+    public void DeleteRezerwacja(int id)
+    {
+        var item = GetRezerwacja(id);
+        if (item != null)
+        {
+            db.Rezerwacje.Remove(item);
+        }
     }
 }
